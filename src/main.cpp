40a075c76e76d5aef60741dbf6ce1b97443cb9ac
Urs Hofmann <hofmannu@ethz.ch>
2024-07-11 22:58:27 +0200

    Adding Backward stack tracing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 // main starting point of the graphical user interface of the application
 
 #include "Interface.h"
+#include <backward/backward.hpp>
+
+backward::SignalHandling sh{};
 
 int main(int *argcp, char**argv)
 {
